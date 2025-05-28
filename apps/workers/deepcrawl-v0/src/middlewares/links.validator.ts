e58b8a8c4f7b850f0c237e445519e24cb254b865
Felix Lyu <felixlyu1018@gmail.com>
2025-05-29 01:06:56 +0800

    worker:ref: update routes validator openapi configs

--- apps/workers/deepcrawl-v0/src/middlewares/links.validator.ts
+++ apps/workers/deepcrawl-v0/src/middlewares/links.validator.ts
@@ -5,7 +5,14 @@ import { targetUrlHelper } from '@/utils/url/target-url-helper';
 
 // Extend the base schema and override only the url field to use targetUrlHelper
 export const LinksOptionsSchema = baseLinkOptionsSchema.extend({
-  url: z.string().transform((url) => {
-    return targetUrlHelper(url);
-  }),
+  url: z
+    .string()
+    .transform((url) => {
+      return targetUrlHelper(url);
+    })
+    .openapi({
+      examples: ['example.com', 'https://example.com'],
+      description:
+        'The valid URL to extract links from. This URL will be validated and normalized.',
+    }),
 });
--- apps/workers/deepcrawl-v0/src/middlewares/read.validator.ts
+++ apps/workers/deepcrawl-v0/src/middlewares/read.validator.ts
@@ -5,7 +5,14 @@ import { targetUrlHelper } from '@/utils/url/target-url-helper';
 
 // Create a version of the schema without the url field
 export const ReadOptionsSchema = baseReadOptionsSchema.extend({
-  url: z.string().transform((url) => {
-    return targetUrlHelper(url);
-  }),
+  url: z
+    .string()
+    .transform((url) => {
+      return targetUrlHelper(url);
+    })
+    .openapi({
+      examples: ['example.com', 'https://example.com'],
+      description:
+        'The valid URL to read. This URL will be validated and normalized.',
+    }),
 });
