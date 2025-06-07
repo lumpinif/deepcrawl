848e012e68a647b2b8b245758451e719f74a228f
Felix Lyu <felixlyu1018@gmail.com>
2025-06-07 18:22:58 +0800

    wks:auth: add envs

--- apps/workers/auth/src/lib/hono/create-hono-app.ts
+++ apps/workers/auth/src/lib/hono/create-hono-app.ts
@@ -4,15 +4,14 @@ import {
   authContextMiddleware,
   authInstanceMiddleware,
 } from '@/middlewares/auth';
-import { corsMiddleware } from '@/middlewares/cors';
 import type { AppBindings } from '@/types';
+import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 import { requestId } from 'hono/request-id';
 import { secureHeaders } from 'hono/secure-headers';
 import { trimTrailingSlash } from 'hono/trailing-slash';
 import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares';
-import { cors } from 'hono/cors';
 
 export const allowedOrigins = [
   'https://auth.deepcrawl.dev',
@@ -25,15 +24,18 @@ export const allowedOrigins = [
 export default function createHonoApp() {
   const app = new Hono<AppBindings>();
 
-  app.use('*', cors({
-    maxAge: 600,
-    credentials: true,
-    origin: allowedOrigins,
-    exposeHeaders: ['Content-Length'],
-    allowMethods: ['POST', 'GET', 'OPTIONS'],
-    allowHeaders: ['Content-Type', 'Authorization'],
-  }))
-  
+  app.use(
+    '*',
+    cors({
+      maxAge: 600,
+      credentials: true,
+      origin: allowedOrigins,
+      exposeHeaders: ['Content-Length'],
+      allowMethods: ['POST', 'GET', 'OPTIONS'],
+      allowHeaders: ['Content-Type', 'Authorization'],
+    }),
+  );
+
   app
     .use(logger())
     .use('*', requestId())
@@ -42,14 +44,13 @@ export default function createHonoApp() {
     .use('*', serveEmojiFavicon('🗝️'))
     .use('*', authInstanceMiddleware)
     .use('*', prettyJSON());
-    
-    app.use('*', authInstanceMiddleware).use('*', authContextMiddleware);
 
-    /* Mount the handler, the path should be synced with the configs in auth.worker.ts */
-    app.on(['POST', 'GET'], '/api/auth/*', (c) => {
-      return c.var.betterAuth.handler(c.req.raw);
-    });
+  app.use('*', authInstanceMiddleware).use('*', authContextMiddleware);
 
+  /* Mount the handler, the path should be synced with the configs in auth.worker.ts */
+  app.on(['POST', 'GET'], '/api/auth/*', (c) => {
+    return c.var.betterAuth.handler(c.req.raw);
+  });
 
   app.onError(onError);
   app.notFound(notFound);
