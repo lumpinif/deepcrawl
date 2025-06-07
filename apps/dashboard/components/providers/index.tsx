2f89d97d83ba9ddc7dca165db109c2caa8809d2b
Felix Lyu <felixlyu1018@gmail.com>
2025-06-07 16:34:23 +0800

    ref: update providers

--- apps/dashboard/components/providers/index.tsx
+++ apps/dashboard/components/providers/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { queryClient } from '@/utils/query.client';
+import { queryClient } from '@/components/providers/query.client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ReactNode } from 'react';
