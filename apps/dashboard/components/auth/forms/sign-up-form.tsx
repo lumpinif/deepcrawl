32d3e295ff4fec71459e002d88dd77691fd3ecdf
Felix Lyu <felixlyu1018@gmail.com>
2025-06-13 22:26:44 +0800

    dash: run check

--- apps/dashboard/components/auth/forms/sign-up-form.tsx
+++ apps/dashboard/components/auth/forms/sign-up-form.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import type { BetterFetchOption } from 'better-auth/react';
 import { useCallback, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
