f04f08cd64dcd29f557d055ce78d1f5a9e77e429
Felix Lyu <felixlyu1018@gmail.com>
2025-06-15 22:16:37 +0800

    wks:auth: update auth worker with updated cors configs and remove unused auth routes

--- apps/workers/auth/src/index.ts
+++ apps/workers/auth/src/index.ts
@@ -1,12 +1,9 @@
 import createHonoApp from './lib/hono/create-hono-app';
-import authRouter from './router/auth';
 
 const app = createHonoApp();
 
 app.get('/', (c) => {
   return c.text('Hello Deepcrawl');
 });
-
-app.route('/', authRouter);
 
 export default app;
--- apps/workers/auth/src/lib/hono/create-hono-app.ts
+++ apps/workers/auth/src/lib/hono/create-hono-app.ts
@@ -5,7 +5,10 @@ import {
   authInstanceMiddleware,
 } from '@/middlewares/auth';
 import type { AppBindings } from '@/types';
-import { ALLOWED_ORIGINS } from '@deepcrawl/auth/configs/auth.config';
+import {
+  ALLOWED_ORIGINS,
+  DEVELOPMENT_ORIGINS,
+} from '@deepcrawl/auth/configs/auth.config';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
@@ -17,17 +20,23 @@ import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares';
 export default function createHonoApp() {
   const app = new Hono<AppBindings>();
 
-  app.use(
-    '*',
+  app.use('*', async (c, next) => {
+    const isDevelopment = c.env.AUTH_WORKER_NODE_ENV === 'development';
+    const allowedOrigins = ALLOWED_ORIGINS;
+    const origin = isDevelopment
+      ? [...allowedOrigins, ...DEVELOPMENT_ORIGINS]
+      : allowedOrigins;
+
     cors({
+      origin,
       maxAge: 600,
       credentials: true,
-      origin: ALLOWED_ORIGINS,
       exposeHeaders: ['Content-Length'],
       allowMethods: ['POST', 'GET', 'OPTIONS'],
       allowHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
+    });
+    await next();
+  });
 
   app
     .use(logger())
