befad48a0e5b6754d47f56250710b08dda2d0582
Felix Lyu <felixlyu1018@gmail.com>
2025-06-22 23:49:29 +0800

    chore: remove www and update code-workspace

--- apps/www/next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@deepcrawl/ui'],
-};
-
-export default nextConfig;
--- apps/www/postcss.config.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from '@deepcrawl/ui/postcss.config';
