c4f568479bfaee1a811ba9d38ea13ffdc83e736e
Felix Lyu <felixlyu1018@gmail.com>
2025-06-26 17:49:40 +0800

    app:ref: hide hover highlight in mobile

--- apps/app/components/app-nav-tabs/index.tsx
+++ apps/app/components/app-nav-tabs/index.tsx
@@ -148,7 +148,7 @@ export default function AppNavTabs() {
       <div className="relative flex w-full flex-nowrap items-center justify-start bg-background-subtle py-2 shadow-none">
         {/* Hover Highlight */}
         <div
-          className="absolute flex h-[30px] items-center rounded-[6px] bg-accent transition-all duration-200 ease-out"
+          className="hidden absolute md:flex h-[30px] items-center rounded-[6px] bg-accent transition-all duration-200 ease-out"
           style={computedHoverStyle}
         />
 
