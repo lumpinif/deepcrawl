0521e97e46637bddd1ec459c5cbd13b96a8c8077
Felix Lyu <felixlyu1018@gmail.com>
2025-07-20 11:35:59 +0800

    app:feat: add logging utility functions for development environment

---
+++ apps/workers/v0/src/utils/loggers.ts
@@ -0,0 +1,32 @@
+import { env as envWorker } from 'cloudflare:workers';
+import type { AppBindings } from '@/lib/context';
+
+export const logDebug = (
+  envProp?: AppBindings['Bindings'],
+  ...args: unknown[]
+) => {
+  const env = envProp?.WORKER_NODE_ENV || envWorker.WORKER_NODE_ENV;
+  if (env === 'development') {
+    console.log(...args);
+  }
+};
+
+export const logWarn = (
+  envProp?: AppBindings['Bindings'],
+  ...args: unknown[]
+) => {
+  const env = envProp?.WORKER_NODE_ENV || envWorker.WORKER_NODE_ENV;
+  if (env === 'development') {
+    console.warn(...args);
+  }
+};
+
+export const logError = (
+  envProp?: AppBindings['Bindings'],
+  ...args: unknown[]
+) => {
+  const env = envProp?.WORKER_NODE_ENV || envWorker.WORKER_NODE_ENV;
+  if (env === 'development') {
+    console.error(...args);
+  }
+};
