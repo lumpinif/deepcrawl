8c4cb3566c5dd5d544c59b233f1de6390d1c8584
Felix Lyu <felixlyu1018@gmail.com>
2025-08-01 12:45:14 +0800

    app:ref: comment out queryCache configuration in makeQueryClient

--- apps/app/lib/query.client.ts
+++ apps/app/lib/query.client.ts
@@ -1,34 +1,31 @@
 import {
   defaultShouldDehydrateQuery,
   isServer,
-  QueryCache,
   QueryClient,
-  type QueryKey,
 } from '@tanstack/react-query';
-import { toast } from 'sonner';
 
 export function makeQueryClient() {
   const client = new QueryClient({
-    queryCache: new QueryCache({
-      onError: (error, query) => {
-        console.error(`❌ Query Error: ${error.message}`, query);
-        if (error.message) {
-          toast.error(error.message, {
-            action: {
-              label: 'retry',
-              onClick: () => {
-                const queryKey = query.queryKey as QueryKey;
-                if (queryKey) {
-                  client.invalidateQueries({ queryKey });
-                } else {
-                  client.invalidateQueries();
-                }
-              },
-            },
-          });
-        }
-      },
-    }),
+    // queryCache: new QueryCache({
+    //   onError: (error, query) => {
+    //     console.error(`❌ Query Error: ${error.message}`, query);
+    //     if (error.message) {
+    //       toast.error(error.message, {
+    //         action: {
+    //           label: 'retry',
+    //           onClick: () => {
+    //             const queryKey = query.queryKey as QueryKey;
+    //             if (queryKey) {
+    //               client.invalidateQueries({ queryKey });
+    //             } else {
+    //               client.invalidateQueries();
+    //             }
+    //           },
+    //         },
+    //       });
+    //     }
+    //   },
+    // }),
     defaultOptions: {
       queries: {
         // Optimized stale time for better performance with cookie caching
