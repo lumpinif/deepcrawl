48930a99949f6e324f505b3d92d2160985854f6a
Felix Lyu <felixlyu1018@gmail.com>
2025-08-31 17:55:35 +0800

    pkg:d1:ref: rename createD1DB to createDBD1 for consistency in database function naming

--- packages/db/db-d1/src/db/index.ts
+++ packages/db/db-d1/src/db/index.ts
@@ -1,7 +1,7 @@
 import type { D1Database } from '@cloudflare/workers-types';
 import { drizzle } from 'drizzle-orm/d1';
 
-export const createD1DB = (d1: D1Database) => {
+export const createDBD1 = (d1: D1Database) => {
   return drizzle(d1);
 };
 
