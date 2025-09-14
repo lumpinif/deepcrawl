a60b7e93473d692956b5bf04c90313997a2010a1
Felix Lyu <felixlyu1018@gmail.com>
2025-09-15 00:28:17 +0800

    app:feat: update dashboard page layout and integrate TaskInput component
    - Commented out the PageHeader component to simplify the dashboard layout.
    - Enhanced the PageTitle with a new description and adjusted styling for better presentation.
    - Integrated the TaskInput component to improve user interaction within the dashboard.

--- apps/app/app/(dashboard)/app/page.tsx
+++ apps/app/app/(dashboard)/app/page.tsx
@@ -1,15 +1,22 @@
 import {
   PageContainer,
-  PageHeader,
+  // PageHeader,
   PageTitle,
 } from '@/components/page-elements';
+import { TaskInput } from '@/components/playground/task-input';
 
 export default async function DashboardPage() {
   return (
     <>
-      <PageHeader title="Dashboard" />
-      <PageContainer>
-        <PageTitle title="Overview" titleSize="2xl" />
+      {/* <PageHeader title="Dashboard" /> */}
+      <PageContainer className="mt-40">
+        <PageTitle
+          className="mx-auto w-full text-center"
+          description="API Playground for Deepcrawl"
+          title="What would you like to see?"
+          titleSize="3xl"
+        />
+        <TaskInput />
       </PageContainer>
     </>
   );
