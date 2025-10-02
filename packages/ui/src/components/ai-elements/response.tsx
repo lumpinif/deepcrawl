6ea9ef1e855a29174833fe8377b7941665321ea8
Felix Lyu <felixlyu1018@gmail.com>
2025-10-02 23:58:13 +0800

    ui:feat: add CustomImage component to Response for enhanced image rendering in Streamdown

--- packages/ui/src/components/ai-elements/response.tsx
+++ packages/ui/src/components/ai-elements/response.tsx
@@ -6,13 +6,31 @@ import { Streamdown } from 'streamdown';
 
 type ResponseProps = ComponentProps<typeof Streamdown>;
 
+const CustomImage = ({
+  src,
+  alt,
+  ...props
+}: {
+  src?: string;
+  alt?: string;
+}) => (
+  <span className="group relative inline-block" data-streamdown="image-wrapper">
+    <img alt={alt} src={src} {...props} />
+    <span className="pointer-events-none absolute inset-0 hidden rounded-lg bg-black/10 group-hover:block" />
+  </span>
+);
+
 export const Response = memo(
-  ({ className, ...props }: ResponseProps) => (
+  ({ className, components, ...props }: ResponseProps) => (
     <Streamdown
       className={cn(
         'size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0',
         className,
       )}
+      components={{
+        img: CustomImage,
+        ...components,
+      }}
       {...props}
     />
   ),
