477c626834f5c99640488c879310489b682541e7
Felix Lyu <felixlyu1018@gmail.com>
2025-10-09 20:39:40 +0800

    app:ref: change IndexPage to a synchronous function for improved performance

--- apps/app/app/(www)/page.tsx
+++ apps/app/app/(www)/page.tsx
@@ -1,4 +1,4 @@
-export default async function IndexPage() {
+export default function IndexPage() {
   return (
     <div className="container flex size-full items-center justify-center border">
       <h1 className="font-bold text-4xl">Hello Deepcrawl</h1>
