4524fd58029b3ded634bc888592544a8a293e4c8
Felix Lyu <felixlyu1018@gmail.com>
2025-10-10 22:57:12 +0800

    app:ref: disable SiteHeader and auth session retrieval in docs layout for temporary adjustments

--- apps/app/app/(docs)/docs/layout.tsx
+++ apps/app/app/(docs)/docs/layout.tsx
@@ -1,22 +1,22 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
-import { SiteHeader } from '@/components/site-header';
+// import { SiteHeader } from '@/components/site-header';
 import { docsOptions } from '@/lib/layout.config';
-import { authGetSession } from '@/query/auth-query.server';
+// import { authGetSession } from '@/query/auth-query.server';
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const currentSession = await authGetSession();
+  // const currentSession = await authGetSession(); // DISABLE CUSTOM SITEHEADER FOR DOCS PAGE FOR NOW
 
   return (
     <DocsLayout {...docsOptions}>
-      <SiteHeader
+      {/* <SiteHeader
         className="sticky top-0 z-50 w-full bg-background max-sm:hidden"
         enableDocsLink={false}
         enableLayoutViewToggle={false}
         enableTitle={false}
         navigationMode="header"
         session={currentSession}
-      />
+      /> */}
       {children}
     </DocsLayout>
   );
