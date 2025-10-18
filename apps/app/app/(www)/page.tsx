a80d2085b89524e8020c8fc7a5d63cdc038fac72
Felix Lyu <felixlyu1018@gmail.com>
2025-10-18 17:05:48 +0800

    app:chore: remove commented Header component from IndexPage for cleaner code

--- apps/app/app/(www)/page.tsx
+++ apps/app/app/(www)/page.tsx
@@ -3,7 +3,6 @@ import { Hero } from '@/components/www/hero';
 export default function IndexPage() {
   return (
     <div className="container mx-auto size-full max-w-6xl divide-y px-0 sm:border-x">
-      {/* <Header /> */}
       <Hero />
     </div>
   );
