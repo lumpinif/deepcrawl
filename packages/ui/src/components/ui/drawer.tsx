cf21c951425e014e7a29e4a90ec13620ce7dc2e8
Felix Lyu <felixlyu1018@gmail.com>
2025-10-22 21:21:42 +0800

    pkg:ui:ref: enhance Drawer component with default class names for improved mobile layout consistency

--- packages/ui/src/components/ui/drawer.tsx
+++ packages/ui/src/components/ui/drawer.tsx
@@ -50,12 +50,17 @@ function DrawerContent({
   children,
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Content>) {
+
+  const defaultCN = 'max-md:px-8 max-md:pb-8 md:max-w-2xl xl:max-w-4xl' as const;
+
+
   return (
     <DrawerPortal data-slot="drawer-portal">
       <DrawerOverlay />
       <DrawerPrimitive.Content
         data-slot="drawer-content"
         className={cn(
+          defaultCN,
           "group/drawer-content bg-background fixed z-50 flex h-auto flex-col",
           "data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b",
           "data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t",
